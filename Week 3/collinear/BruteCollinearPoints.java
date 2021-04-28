efb8f88c497fb8a382e32ab47694dbb1867add83
yanxuanshaozhu <973493269@qq.com>
2021-04-28 23:16:59 +0800

    Upload files

---
+++ Week 3/collinear/BruteCollinearPoints.java
@@ -0,0 +1,22 @@
+/* *****************************************************************************
+ *  Name: yanxuanshaozhu
+ *  Date: 04/27/2021
+ *  Description:
+ **************************************************************************** */
+
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+    // the number of line segments
+    public int numberOfSegments() {
+
+    }
+
+    // the line segments
+    public LineSegment[] segments() {
+
+    }
+}
